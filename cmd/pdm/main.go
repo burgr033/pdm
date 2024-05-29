ab7c272fd0b4687a6751be6cc8adb190da70d4ed
sb <simon@burgr.io>
2024-05-29 14:50:14 +0200

    bump

--- cmd/pdm/main.go
+++ cmd/pdm/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	"github.com/burgr033/pdm/internal/config"
+	"github.com/burgr033/pdm/internal/git"
 )
 
 // main is the entry point of the pdm command line tool.
 func main() {
-	conf := config.ReadConfig()
+	git.CloneAction("https://github.com/burgr033/asdf")
+	git.CloneAction("https://github.com/burgr033/dotfiles")
+	git.CloneAction("asdf")
 }
