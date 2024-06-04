f3b09a16bea6527e8c3e3729d323293c4439c8d3
sb <simon@burgr.io>
2024-06-04 14:26:47 +0200

    added config struct for projects

--- internal/config/config.go
+++ internal/config/config.go
@@ -8,10 +8,32 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
+// Config represents the structure of the config file.
+// It contains the project directory path, a list of categories, and a list of projects.
 type Config struct {
-	ProjectDirectory string `yaml:"project_directory"`
+	ProjectDirectory string     `yaml:"project_directory"`
+	Categories       []Category `yaml:"categories"`
+	Projects         []Project  `yaml:"projects"`
 }
 
+// Project represents the structure of a project.
+// It contains the name, category, origin, and state of the project.
+type Project struct {
+	Name     string `yaml:"name"`
+	Category string `yaml:"category"`
+	Origin   string `yaml:"origin"`
+	State    string `yaml:"state"`
+}
+
+// Category represents the structure of a category.
+// It contains the name and relative path of the category.
+type Category struct {
+	Name    string `yaml:"name"`
+	RelPath string `yaml:"rel_path"`
+}
+
+// getConfigPath returns the path to the config file.
+// exported = no
 func getConfigPath() string {
 	config, err := xdg.ConfigFile("pdm/config.yaml")
 	if err != nil {
@@ -20,6 +42,23 @@ func getConfigPath() string {
 	return config
 }
 
+// WriteConfig writes the config to the config file.
+// It takes a pointer to a Config struct as an argument.
+// exported = yes
+func WriteConfig(conf *Config) {
+	data, err := yaml.Marshal(conf)
+	if err != nil {
+		log.Fatalf("Marshal: %v", err)
+	}
+	err = os.WriteFile(getConfigPath(), data, 0644)
+	if err != nil {
+		log.Fatalf("WriteFile: %v", err)
+	}
+}
+
+// ReadConfig reads the config from the config file.
+// It returns a pointer to a Config struct.
+// exported = yes
 func ReadConfig() *Config {
 	yamlFile, err := os.ReadFile(getConfigPath())
 	if err != nil {
